43d215a2b079031fa7a4aa629e1d76b0b68f901f
Jake Jones <jjones2797@gmail.com>
2026-05-21 15:13:14 -0400

    Initial Commit

---
+++ Vara/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vara
+//
+//  Created by Jake Jones on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vara/VaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaraApp.swift
+//  Vara
+//
+//  Created by Jake Jones on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct VaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
